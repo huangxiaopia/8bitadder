bf22554ad36eaa71c40014a2ad5386dced83183e
dQwaz <56117011+dQwaz@users.noreply.github.com>
2021-03-16 23:09:19 +0100

    Add files via upload
    1st commit - start function with menu and termination statement

---
+++ bitadder.py
@@ -0,0 +1,8 @@
+# from BinPy import *
+# Starting the bit adder
+def start():
+    print("8bit adder is active")
+    start_input = input('Please select an option\n1 : Start \n2 : Exit\nYour choice : ')
+    if start_input == '2' and start_input == '':
+        exit()
+    return start_input
